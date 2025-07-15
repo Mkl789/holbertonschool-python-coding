284c06a4dc937d6efe62d654a39d1b1344b1c97e
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-16 10:18:05 +1200

    gh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,10 +5,5 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size=0):
-        """Initializes the square with an optional size."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    def __init__(self, size):
         self.__size = size
