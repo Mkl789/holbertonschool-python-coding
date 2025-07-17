ccd6b06d49bab15f59839e0ad5630a8f921859d2
Mkl789 <mikaeel.lalmahomed@gmail.com>
2025-07-17 13:30:45 +1200

    m

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     """
-    Defines if the object is exactly an instance of the specified class
-    Checks for exact type match (and return True for a match)
+    Defines if the object is exactly an instance of the specified class.
+    Returns True if exact match, otherwise False.
     """
-
     return type(obj) == a_class
